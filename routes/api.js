9d69f9c95c89837ef2ff26182dadff2f73739983
Israel Adura <israeladura@gmail.com>
2018-08-19 21:14:02 +0100

    *

--- routes/api.js
+++ routes/api.js
@@ -15,37 +15,38 @@ router.post('/file', (req, res, next) => {
   let {source, destination} = req.body;
 
   console.log(req.body)
-  // const filename = returnPath(source);
+  const filename = returnPath(source);
   // console.log(filename)
   // console.log(source, destination)
 
-  // const fileStream = fs.createWriteStream(`uploads/${filename}`);
+  const fileStream = fs.createWriteStream(`uploads/${filename}`);
 
-  // //create a file by piping into it data from a file's url
-  // request
-  //   .get(source)
-  //   .on('error', (err) => {
-  //     if (err) 
-  //       throw err;
-  //     console.log(err)
-  //   })
-  //   .pipe(fileStream);
+  //create a file by piping into it data from a file's url
+  request
+    .get(source)
+    .on('error', (err) => {
+      if (err) 
+        throw err;
+      console.log(err)
+    })
+    .pipe(fileStream);
 
-  // console.log(fileStream)
+  //console.log(fileStream)
 
-  // // send to apt destination on server using ftp
-  // const ftp = new PromiseFtp();
-  // ftp
-  //   .connect({host: ftpCreds.host, user: ftpCreds.user, password: ftpCreds.pass})
-  //   .then(function (serverMessage) {
-  //     console.log(serverMessage)
-  //     //return ftp.list('/');
-  //     return ftp.put(`uploads/${filename}`, `/${destination}/${filename}`);
-  //   })
-  //   .then(function () {
-  //     return ftp.end();
-  //     console.log('ended')
-  //   });
+  // send to apt destination on server using ftp
+  const ftp = new PromiseFtp();
+  ftp
+    .connect({host: ftpCreds.host, user: ftpCreds.user, password: ftpCreds.pass})
+    .then(function (serverMessage) {
+      console.log(serverMessage)
+      console.log('connected')
+      //return ftp.list('/');
+      return ftp.put(`uploads/${filename}`, `/${destination}/${filename}`);
+    })
+    .then(function () {
+      return ftp.end();
+      console.log('ended')
+    });
 })
 
 /*********HELPERS*********/
